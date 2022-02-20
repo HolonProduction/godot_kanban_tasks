504be0df906dab512a1568024db7bd78135e1a08
HolonProduction <holonproduction@gmail.com>
2022-02-20 19:56:51 +0100

    Update internal documentation

---
+++ addons/kanban_tasks/documentation.gd
@@ -0,0 +1,9 @@
+extends AcceptDialog
+
+onready var label := $VBoxContainer/RichTextLabel
+
+func _ready():
+	label.connect("meta_clicked", self, "__label_meta_clicked")
+
+func __label_meta_clicked(meta):
+	OS.shell_open(meta)
